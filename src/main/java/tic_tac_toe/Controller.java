3b3d6aa55fb462ede835f0526793ba0278daf4ff
Ramon Medeiros <ramon.rnm@gmail.com>
2019-05-03 16:19:51 +0200

    adding rest features and implementing tests

--- src/main/java/tic_tac_toe/Controller.java
+++ src/main/java/tic_tac_toe/Controller.java
@@ -4,221 +4,155 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.json.simple.JSONObject;;
 
 @RestController
 public class Controller {
 	private HashMap<String, Game> games = new HashMap<String, Game>();
 	private HashMap<String, String> users = new HashMap<String, String>();
-	private String GET = "GET";
-	private String POST = "POST";
-	private String DELETE = "DELETE";
-	private String TITLE = "Tic Tac Toe by Ramon Medeiros";
-	private String USERNAME = "username";
+	final String TITLE = "Tic Tac Toe by Ramon Medeiros";
+	final String USERNAME = "username";
+	final String TOKEN = "token";
+	final String LINE = "line";
+	final String COLUMN = "column";
+	final String PLAYER = "player";
 	private static final Logger LOGGER = Logger.getLogger(Game.class.getName());
 
-    @RequestMapping("/")
-    public String index() {
-        return TITLE;
-    }
+	@RequestMapping("/")
+	public String index() {
+		return TITLE;
+	}
 
-    // Total control - setup a model and return the view name yourself. Or
-    // consider subclassing ExceptionHandlerExceptionResolver (see below).
-    @ExceptionHandler(Exception.class)
-    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
-      LOGGER.info("Request: " + req.getRequestURL() + " raised " + ex);
+	// Total control - setup a model and return the view name yourself. Or
+	// consider subclassing ExceptionHandlerExceptionResolver (see below).
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+		LOGGER.info("Request: " + req.getRequestURL() + " raised " + ex);
 
-      ModelAndView mav = new ModelAndView();
-      mav.addObject("exception", ex);
-      mav.setViewName("error");
-      return mav;
-    }
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("exception", ex);
+		mav.setViewName("error");
+		return mav;
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST, params = { USERNAME }, produces = "application/json")
+	public String login(@RequestParam(USERNAME) String username) {
+		String token = UUID.randomUUID().toString();
+		this.users.put(username, token);
+		JSONObject ret = new JSONObject();
+		ret.put(TOKEN, token);
+		return ret.toString();
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.POST, params = { USERNAME,
+			TOKEN }, produces = "application/json")
+	public ResponseEntity<String> logout(@RequestParam(USERNAME) String username, @RequestParam(TOKEN) String token) {
+		if (this.users.get(username).compareTo(token) == 0) {
+			this.users.remove(username);
+			return new ResponseEntity<>("accepted", HttpStatus.OK);
+		}
+		return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND);
+	}
+
+	@RequestMapping(value = "/game", method = RequestMethod.GET)
+	public String gameGET() {
+		return this.games.keySet().toString();
+	}
+
+	@RequestMapping(value = "/game", method=RequestMethod.POST,params = {TOKEN}, produces = "application/json")
+	public ResponseEntity<String> gamePOST(@RequestParam(TOKEN) String token) {
+		  if (this.users.containsValue(token) == false) {
+			  return new ResponseEntity<>("invalid token", HttpStatus.UNAUTHORIZED);
+		  }
+		  Game game = new Game();
+		  this.games.put(game.game_id, game);
+		  return new ResponseEntity<>(game.game_id, HttpStatus.OK); 
+	}
+
+	@RequestMapping(value = "/game/**", method=RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<String> getGameUuid(@PathVariable("uuid") String uuid) {
+		if (this.games.containsKey(uuid) == false) {
+			return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND); 
+		}
+		
+		JSONObject resp = new JSONObject();
+		resp.put("board", this.games.get(uuid).getBoard());
+		resp.put("winner", this.games.get(uuid).getWinner());
+		resp.put("players", this.games.get(uuid).getPlayers());
+		return new ResponseEntity<>(resp.toJSONString(), HttpStatus.OK); 
+	}
+	
+	@RequestMapping(value = "/game/**", method=RequestMethod.POST, params = {TOKEN, LINE, COLUMN}, produces = "application/json")
+	public ResponseEntity<String> postGameUuid(@PathVariable("uuid") String uuid, @RequestParam(TOKEN) String token, @RequestParam(LINE) String line, @RequestParam(COLUMN) String column) {
+		if (this.games.containsKey(uuid) == false) {
+			return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND); 
+		}
+		
+		 if (this.users.containsValue(token) == false) {
+			  return new ResponseEntity<>("invalid token", HttpStatus.UNAUTHORIZED);
+		  }
+		
+		if (this.games.get(uuid).doMove(line, column, token) == true) {
+			return new ResponseEntity<>("moved", HttpStatus.OK);
+		}
+		return new ResponseEntity<>("failed", HttpStatus.FORBIDDEN);
+	}
+	
+	
+	@RequestMapping(value = "/game/**", method=RequestMethod.DELETE, params = {TOKEN}, produces = "application/json")
+	public ResponseEntity<String> deleteGameUuid(@PathVariable("uuid") String uuid, @RequestParam(TOKEN) String token) {
+		if (this.games.containsKey(uuid) == false) {
+			return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND); 
+		}
+		if (this.users.containsValue(token) == false) {
+			return new ResponseEntity<>("invalid token", HttpStatus.UNAUTHORIZED);
+		}
+		
+		this.games.remove(uuid);
+		return new ResponseEntity<>("deleted", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/game/{uuid}/player", method=RequestMethod.POST, params = {TOKEN, PLAYER}, produces = "application/json")
+	public ResponseEntity<String> addPlayer(@PathVariable("uuid") String uuid, @RequestParam(TOKEN) String token, @RequestParam(PLAYER) String player) {
+		if (this.games.containsKey(uuid) == false) {
+			return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND); 
+		}
+		if (this.users.containsValue(token) == false) {
+			return new ResponseEntity<>("invalid token", HttpStatus.UNAUTHORIZED);
+		}
+		
+		// check if player is already
+		if (this.games.get(uuid).getPlayers().contains(player) == true) {
+			return new ResponseEntity<>(String.format("Player %s its already being used", player), HttpStatus.BAD_REQUEST); 
+		}
+		
+		this.games.get(uuid).players.put(player, token);
+		return new ResponseEntity<>("done", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/game/{uuid}/player", params = {TOKEN}, method=RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<String> getPlayers(@PathVariable("uuid") String uuid, @RequestParam(TOKEN) String token) {
+		if (this.games.containsKey(uuid) == false) {
+			return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND); 
+		}
+		if (this.users.containsValue(token) == false) {
+			return new ResponseEntity<>("invalid token", HttpStatus.UNAUTHORIZED);
+		}
+		
+		return new ResponseEntity<>(this.games.get(uuid).getPlayerByToken(token) , HttpStatus.OK);
+	}
+
+	
 }
-    
- /*   @RequestMapping("/login")
-    @app.route("/login", methods=[POST])
-    def login():
-        global users
-        rjson = request.json
-        if validate_params(rjson, [USERNAME]) is False or len(rjson.get(USERNAME)) == 0:
-            return "invalid or inexistent username", 404
-        user = rjson[USERNAME]
-        
-        token = uuid4().__str__()
-        users[token] = user
-        return jsonify({TOKEN:token})
-}
-
-/*
-
-
-
-
-@app.before_request
-def only_json():
-    if request.method == POST and not request.is_json: 
-        return "not json", 400
-
-
-
-@app.route("/logout", methods=[POST])
-def logout():
-    rjson = request.json
-    if validate_params(rjson, [TOKEN, USERNAME]) is False or len(rjson.get(USERNAME)) == 0:
-        return "invalid or inexistent username", 404
-
-    user = rjson[USERNAME]
-    token = rjson[TOKEN]
-    
-    if validate_user(user, token) is True:
-        users.pop(token)
-        return "accepted", 200
-
-    return "not found", 404
- 
-
-@app.route("/game", methods=[GET, POST])
-def game():
-    global games
-
-    if request.method == POST:
-        if TOKEN not in request.json:
-            return "expected token", 404
-
-        token = request.json[TOKEN]
-        if users.get(token) == None:
-            return "invalid token", 404
-
-        game = Game()
-        game_uuid = uuid4().__str__()
-        games[game_uuid] = game
-        return game_uuid, 201
-
-    elif request.method == GET:
-        return jsonify(list(games.keys()))
-
-    return f"{request.method} not implemented", 405
-
-
-@app.route("/game/<uuid>", methods=[GET, POST, DELETE])
-def deal_with_game(uuid: str):
-    global games
-
-    # check if uuid is valid
-    this_game = games.get(uuid)
-
-    if this_game is None:
-        return "not found", 404
-
-    if request.method == POST:
-
-        rjson = request.json
-        if validate_request_json(rjson) is not True:
-            return "invalid params", 406
-
-        if this_game.do_move(rjson[LINE], rjson[COLUMN],
-                             rjson[TOKEN]) is True:
-            return "moved", 200
-        return "failed", 403
-
-    if request.method == GET:
-        return jsonify({
-            "board": this_game.get_board(),
-            "winner": this_game.get_winner(),
-            "players": this_game.get_players()
-        })
-
-    elif request.method == DELETE:
-        return games.delete(request.form["uuid"])
-
-    return f"{request.method} not implemented", 405
-
-@app.route("/game/<uuid>/player", methods=[POST, GET])
-def register_player(uuid: str):
-    global users
-
-    # check if uuid is valid
-    this_game = games.get(uuid)
-
-    if this_game is None:
-        return "not found", 404
-
-    rjson = request.json
-
-    if request.method == POST:
-
-        if validate_params(rjson, [PLAYER, TOKEN]) is False:
-            return "expected player and token", 404
-
-        player = rjson[PLAYER]
-        token = rjson[TOKEN]
-
-        if users.get(token) is None:
-            return "not valid token", 401
-
-        if player in games[uuid].get_players():
-            return f"Player {player} its already being used", 400
-
-        games[uuid]._players[player] = token
-        return "done", 200
-
-    elif request.method == GET:
-        params = request.args.to_dict()
-        if TOKEN not in params:
-            return "expected token", 404
-
-        token = params[TOKEN]
-
-        if users.get(token) is None:
-            return "not valid token", 401
-
-        for player,tk in this_game._players.items():
-            if tk == token:
-                return jsonify({"player": player})
-        return "not found", 404
-
-
-    return f"{request.method} not implemented", 405
-
-
-def validate_user(user, token):
-    global users
-    if users.get(token) != user:
-        return False
-
-    return True
-
-def validate_params(json, expected_headers: list):
-    json_keys = list(json.keys())
-    json_keys.sort()
-    expected_headers.sort()
-
-    if json_keys == expected_headers:
-        return True
-    return False
-
-
-def validate_request_json(rjson):
-    global users
-    if validate_params(rjson, [COLUMN, LINE, TOKEN]) is False:
-        return False
-
-    if users.get(rjson[TOKEN]) == None:
-        raise GameException("Token must be valid")
-
-    if isinstance(rjson[LINE], int) is False:
-        raise GameException("Line must be int")
-
-    if isinstance(rjson[COLUMN], int) is False:
-        raise GameException("Column must be int")
-
-    return True
-
-
-
-*/
--- src/test/java/tic_tac_toe/ControllerTest.java
+++ src/test/java/tic_tac_toe/ControllerTest.java
@@ -1,9 +1,15 @@
 package tic_tac_toe;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,21 +18,99 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=tic_tac_toe.Application.class)
+@SpringBootTest(classes = tic_tac_toe.Application.class)
 @AutoConfigureMockMvc
 public class ControllerTest {
+	String X_token, O_token, gameUUID;
+	final String X_USERNAME = "X-name";
+	final String O_USERNAME = "O-name";
+	final String USERNAME = "username";
+	final String TOKEN = "token";
+	final String PLAYER = "player";
+	JSONParser jsonparser = new JSONParser();
 
-    @Autowired
-    private MockMvc mvc;
+	@Autowired
+	private MockMvc mvc;
 
-    @Test
-    public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Tic Tac Toe by Ramon Medeiros")));
-    }
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Tic Tac Toe by Ramon Medeiros")));
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		// login user X
+		MvcResult Xresponse = mvc
+				.perform(MockMvcRequestBuilders.post("/login").accept(MediaType.APPLICATION_JSON).param(USERNAME,
+						X_USERNAME))
+				.andExpect(status().isOk()).andExpect(content().string(containsString("token"))).andReturn();
+		JSONObject XToken = (JSONObject) jsonparser.parse(Xresponse.getResponse().getContentAsString());
+		this.X_token = (String) XToken.get(TOKEN);
+
+		// login user O
+		MvcResult Oresponse = mvc
+				.perform(MockMvcRequestBuilders.post("/login").accept(MediaType.APPLICATION_JSON).param(USERNAME,
+						O_USERNAME))
+				.andExpect(status().isOk()).andExpect(content().string(containsString("token"))).andReturn();
+		JSONObject OToken = (JSONObject) jsonparser.parse(Oresponse.getResponse().getContentAsString());
+		this.O_token = (String) OToken.get(TOKEN);
+
+		// start game
+		MvcResult newGame = mvc
+				.perform(MockMvcRequestBuilders.post("/game").accept(MediaType.APPLICATION_JSON).param(TOKEN,
+						this.X_token))
+				.andExpect(status().isOk()).andReturn();
+		this.gameUUID = newGame.getResponse().getContentAsString();
+
+		// register player X
+		String gameplayeruri = String.format("/game/%s/player", this.gameUUID);
+		MultiValueMap<String, String> xparams = new LinkedMultiValueMap<>();
+		xparams.add(PLAYER, "X");
+		xparams.add(TOKEN, this.X_token);
+		mvc.perform(MockMvcRequestBuilders.post(gameplayeruri).accept(MediaType.APPLICATION_JSON).params(xparams))
+				.andExpect(status().isOk());
+
+		// register player O
+		MultiValueMap<String, String> oparams = new LinkedMultiValueMap<>();
+		oparams.add(PLAYER, "O");
+		oparams.add(TOKEN, this.O_token);
+		mvc.perform(MockMvcRequestBuilders.post(gameplayeruri).accept(MediaType.APPLICATION_JSON).params(oparams))
+				.andExpect(status().isOk());
+	}
+
+	@After
+	public void logout() throws Exception {
+		// logout user X
+		MultiValueMap<String, String> xparams = new LinkedMultiValueMap<>();
+		xparams.add(USERNAME, X_USERNAME);
+		xparams.add(TOKEN, this.X_token);
+		mvc.perform(MockMvcRequestBuilders.post("/logout").accept(MediaType.APPLICATION_JSON).params(xparams))
+				.andExpect(status().isOk());
+
+		// logout user O
+		MultiValueMap<String, String> oparams = new LinkedMultiValueMap<>();
+		oparams.add(USERNAME, O_USERNAME);
+		oparams.add(TOKEN, this.O_token);
+		mvc.perform(MockMvcRequestBuilders.post("/logout").accept(MediaType.APPLICATION_JSON).params(oparams))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void getPlayerByToken() throws Exception {
+		String gameplayeruri = String.format("/game/%s/player", this.gameUUID);
+		MvcResult newGame = mvc
+				.perform(MockMvcRequestBuilders.get(gameplayeruri).accept(MediaType.APPLICATION_JSON).param(TOKEN,
+						this.X_token))
+				.andExpect(status().isOk()).andReturn();
+		assertEquals(newGame.getResponse().getContentAsString(), "X");
+	}
+
 }
